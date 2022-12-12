f238805b2966971d73f95a6194b200c2bbb7a20d
QuinceyWilliams <112519307+QuinceyWilliams@users.noreply.github.com>
2022-12-11 17:32:28 -0800

    Update pagerank.py

--- pagerank.py
+++ pagerank.py
@@ -2,7 +2,6 @@
 
 '''
 This file calculates pagerank vectors for small-scale webgraphs.
-See the README.md for example usage.
 '''
 
 import math
@@ -10,17 +9,13 @@ import torch
 import gzip
 import csv
 import logging
+import gensim.downloader
+
+vectors = gensim.downloader.load('glove-wiki-gigaword-300')
 
 class WebGraph():
 
-  
     def __init__(self, filename, max_nnz=None, filter_ratio=None):
-        '''
-        Initializes the WebGraph from a file.
-        The file should be a gzipped csv file.
-        Each line contains two entries: the source and target corresponding to a single web link.
-        This code assumes that the file is sorted on the source column.
-        '''
 
         self.url_dict = {}
         indices = []
@@ -50,8 +45,8 @@ class WebGraph():
                 if target_counts[target] < filter_ratio*len(self.url_dict):
                     new_indices.append([source,target])
             indices = new_indices
-            
-        # compute the values that correspond to the indices variable
+
+        # compute the values
         logging.debug('computing values')
         values = []
         last_source = indices[0][0]
@@ -72,43 +67,28 @@ class WebGraph():
         self.P = torch.sparse.FloatTensor(i, v, torch.Size([n,n]))
         self.index_dict = {v: k for k, v in self.url_dict.items()}
 
-        
+
     def _url_to_index(self, url):
-        '''
-        given a url, returns the row/col index into the self.P matrix
-        '''
         if url not in self.url_dict:
             self.url_dict[url] = len(self.url_dict)
         return self.url_dict[url]
 
 
     def _index_to_url(self, index):
-        '''
-        given a row/col index into the self.P matrix, returns the corresponding url
-        '''
         return self.index_dict[index]
 
-      
+
     def make_personalization_vector(self, query=None):
-        '''
-        If query is None, returns the vector of 1s.
-        If query contains a string,
-        then each url satisfying the query has the vector entry set to 1;
-        all other entries are set to 0.
-        '''
         n = self.P.shape[0]
 
         if query is None:
             v = torch.ones(n)
+
         else:
             v = torch.zeros(n)
-            for i in range(n):
-                url = self._index_to_url(i)
-                if url_satisfies_query(url, query) == True:
-                   v[i] = 1
-            v /= torch.norm(v)
-
-            # FIXME: implement  Task 2
+            for url,i in self.url_dict.items():
+                if url_satisfies_query(url, query):
+                    v[i] = 1
 
         v_sum = torch.sum(v)
         assert(v_sum>0)
@@ -116,19 +96,17 @@ class WebGraph():
 
         return v
 
-      
-    def power_method(self, v=None, x0=None, alpha=0.85, max_iterations=1000, epsilon=1e-6):
-        '''
-        This function implements the power method for computing the pagerank.
 
-        The self.P variable stores the $P$ matrix.
-        You will have to compute the $a$ vector and implement Equation 5.1 from "Deeper Inside Pagerank."
-        '''
+    def power_method(self, v=None, x0=None, alpha=0.85, max_iterations=1000, epsilon=1e-6):
         with torch.no_grad():
             n = self.P.shape[0]
 
-            # create variables if none given
+            # compute the a vector
+            nondangling_nodes = torch.sparse.sum(self.P,1).indices()
+            a = torch.ones([n,1])
+            a[nondangling_nodes] = 0
 
+            # create input variables if none given
             if v is None:
                 v = torch.Tensor([1/n]*n)
                 v = torch.unsqueeze(v,1)
@@ -139,43 +117,62 @@ class WebGraph():
                 x0 = torch.unsqueeze(x0,1)
             x0 /= torch.norm(x0)
 
-            a = torch.zeros(n,1)
-            notdangling = torch.sparse.sum(self.P,1).indices()
-            a[notdangling] = 0
-
             # main loop
             xprev = x0
             x = xprev.detach().clone()
             for i in range(max_iterations):
                 xprev = x.detach().clone()
-
-                p = (alpha*xprev.t()@a+(1 - alpha))*v.t()
-                x = torch.sparse.addmm(p.t(), self.P.t(), alpha*xprev, alpha = 0.85)
-
-                # compute the new x vector using Eq (5.1)
-                # FIXME: Task 1
-                # HINT: this can be done with a single call to the `torch.sparse.addmm` function,
-                # but you'll have to read the code above to figure out what variables should get passed to that function
-                # and what pre/post processing needs to be done to them
-
-                # output debug information
-                residual = torch.norm(x-xprev)
-                logging.debug(f'i={i} residual={residual}')
-
-                # early stop when sufficient accuracy reached
-                if residual < epsilon:
+                q = (alpha*x.t()@a + (1-alpha)) * v.t()
+                x = torch.sparse.addmm(
+                        q.t(),
+                        self.P.t(),
+                        x,
+                        beta=1,
+                        alpha=alpha
+                        )
+                x /= torch.norm(x, p=1)
+                accuracy = torch.norm(x-xprev)
+                logging.debug('i='+str(i)+' accuracy='+str(accuracy)+' sum(x)='+str( torch.sum(x)))
+                if accuracy < epsilon:
                     break
 
-            #x = x0.squeeze()
             return x.squeeze()
 
-          
-    def search(self, pi, query='', max_results=10):
+    
+    def search(self, pi, query='', max_results=10, sum_weight=0.03, power=30):
         '''
-        Logs all urls that match the query.
-        Results are displayed in sorted order according to the pagerank vector pi.
+        This function prints the top ranked urls that match the input query.
+        NOTE:
+        For the Task 1, there is no need to modify this code.
+        For the Task 2, then you would have to modify this code.
         '''
         n = self.P.shape[0]
+        k = min(max_results,n)
+
+        S = vectors.most_similar(args.search_query)
+
+        for i in range(n):
+            number = 0
+            score = 0
+            weight = 0
+
+            urls = self._index_to_url(i)
+
+            if new_url_satisfies_query(urls, query):
+                number += 1
+                weight += sum_weight
+
+            for word in range(10):
+                W = S[word][0]
+
+                if new_url_satisfies_query(urls, W):
+                    number += 1
+                    weight += S[word][1]**power
+
+            score += number*weight
+
+            pi[i] += score
+
         vals,indices = torch.topk(pi,n)
 
         matches = 0
@@ -186,10 +183,30 @@ class WebGraph():
             url = self._index_to_url(index)
             pagerank = vals[i].item()
             if url_satisfies_query(url,query):
-                logging.info(f'rank={matches} pagerank={pagerank:0.4e} url={url}')
+                logging.info('rank='+str(matches)+' pagerank='+str(pagerank)+' url='+url)
                 matches += 1
-          
-          
+
+
+def new_url_satisfies_query(url,query):
+    satisfies = False
+    terms = query.split()
+
+    num_terms=0
+    for term in terms:
+        if term[0] != '-':
+            num_terms+=1
+            if term in url:
+                satisfies = True
+    if num_terms==0:
+        satisfies=True
+
+    for term in terms:
+        if term[0] == '-':
+            if term[1:] in url:
+                return False
+    return satisfies
+
+
 def url_satisfies_query(url, query):
     '''
     This functions supports a moderately sophisticated syntax for searching urls for a query string.
@@ -197,7 +214,6 @@ def url_satisfies_query(url, query):
     But, if a word is preceded by the negation sign `-`,
     then the function returns False if that word is present in the url,
     even if it would otherwise return True.
-
     >>> url_satisfies_query('www.lawfareblog.com/covid-19-speech', 'covid')
     True
     >>> url_satisfies_query('www.lawfareblog.com/covid-19-speech', 'coronavirus covid')
@@ -218,6 +234,14 @@ def url_satisfies_query(url, query):
     satisfies = False
     terms = query.split()
 
+    for word in query.split():
+        if '-' in word:
+            pass
+        else:
+            similar_words = vectors.most_similar(word,topn=5)
+            for i in range(len(similar_words)):
+                terms.append(similar_words[i][0])
+
     num_terms=0
     for term in terms:
         if term[0] != '-':
@@ -232,12 +256,13 @@ def url_satisfies_query(url, query):
             if term[1:] in url:
                 return False
     return satisfies
-  
+
+
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', required=True)
-    parser.add_argument('--personalization_vector_query')
+    parser.add_argument('--personalization_vector_query', default='')
     parser.add_argument('--search_query', default='')
     parser.add_argument('--filter_ratio', type=float, default=None)
     parser.add_argument('--alpha', type=float, default=0.85)
@@ -245,6 +270,8 @@ if __name__=='__main__':
     parser.add_argument('--epsilon', type=float, default=1e-6)
     parser.add_argument('--max_results', type=int, default=10)
     parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--power', type=int, default=30)
+    parser.add_argument('--sum_weight', type=float, default=0.03)
     args = parser.parse_args()
 
     if args.verbose:
@@ -255,4 +282,4 @@ if __name__=='__main__':
     g = WebGraph(args.data, filter_ratio=args.filter_ratio)
     v = g.make_personalization_vector(args.personalization_vector_query)
     pi = g.power_method(v, alpha=args.alpha, max_iterations=args.max_iterations, epsilon=args.epsilon)
-    g.search(pi, query=args.search_query, max_results=args.max_results)
+    g.search(pi, query=args.search_query, max_results=args.max_results, sum_weight=args.sum_weight, power=args.power)
